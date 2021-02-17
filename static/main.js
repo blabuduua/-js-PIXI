6bdaf59bd536f6df52d63813104becc7bb8814fc
Doroshenko Maksym <Doroshenko.Maksym@flyuia.com>
2021-02-17 14:51:38 +0200

    image in center, rotate from center

--- static/main.js
+++ static/main.js
@@ -13,50 +13,15 @@ PIXI.Loader.shared
 function setup() {
 	let matrix = new PIXI.Sprite( PIXI.Loader.shared.resources['static/images/matrix.png'].texture );
 
-	matrix.x = 100;
-	matrix.y = 100;
+	matrix.x = 300;
+	matrix.y = 300;
 
-	matrix.anchor.set(0.09);
-
-	app.stage.addChild( matrix );
-
-	let Sprite = PIXI.Sprite;
-	let second_matrix = new Sprite( PIXI.Loader.shared.resources['static/images/matrix.png'].texture );
-
-	second_matrix.x = 50;
-	second_matrix.y = 50;
-
-	second_matrix.scale.set(1.5);
-	second_matrix.anchor.set(0);
+	matrix.scale.set(0.5);
+	matrix.anchor.set(0.5); // ЯКОРЬ, ТОЧКА ПРИВЯЗКИ ОБЬЕКТА (1 - ЛЕВЫЙ ВЕРХНИЙ, 0.5 - ЦЕНТР) 
 
 	app.ticker.add( (delta) => {
-		second_matrix.rotation -= 0.01 * delta;
+		matrix.rotation -= 0.01 * delta;
 	})
 
-	app.stage.addChild( second_matrix );
+	app.stage.addChild( matrix );
 }
-
-
-
-
-
-// const container = new PIXI.Container();
-// app.stage.addChild( container );
-
-// const texture = PIXI.Texture.from('static/images/matrix.png');
-
-// const smile = new PIXI.Sprite( texture );
-// smile.anchor.set(0.5);
-
-// container.addChild( smile );
-
-// container.x = app.screen.width / 2;
-// container.y = app.screen.height / 2;
-
-// container.pivot.x = container.width / 2;
-// container.pivot.y = container.height / 2;
-
-
-// app.ticker.add( (delta) => {
-// 	container.rotation -= 0.01 * delta;
-// })

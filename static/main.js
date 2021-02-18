b1ec781ebdfc83533ab181ba0b936afc7cad12a5
Doroshenko Maksym <Doroshenko.Maksym@flyuia.com>
2021-02-18 10:58:28 +0200

    add load events

--- static/main.js
+++ static/main.js
@@ -1,4 +1,9 @@
-const app = new PIXI.Application({
+let Application = PIXI.Application,
+    loader = PIXI.Loader.shared,
+    resources = PIXI.Loader.shared.resources,
+    Sprite = PIXI.Sprite;
+
+let app = new Application({
 	width: 720,
 	height: 750,
 	backgroundColor: 0x1099bb,
@@ -6,16 +11,24 @@ const app = new PIXI.Application({
 
 document.body.appendChild( app.view );
 
-PIXI.Loader.shared
-	.add("static/images/atlas.json")
-	.load( setup )
+loader
+    .add("atlas", "static/images/atlas.json")
+    .use((resource, next) =>
+    {
+        // Be sure to call next() when you have completed your middleware work.
+        next();
+    })
+    .load((loader, resources) => {
+		let id = resources.atlas.textures;
 
-function setup() {
-	let id = PIXI.Loader.shared.resources["static/images/atlas.json"].textures;
+		let sprite = new Sprite( id["uia_logo_w.png"] );
 
-	console.log(id)
+		app.stage.addChild( sprite );
+    });
 
-	let sprite = new PIXI.Sprite( id["uia_logo_w.png"] );
-
-	app.stage.addChild( sprite );
-}
+// Throughout the process multiple signals can be dispatched.
+loader.onStart.add(() => {console.log("onStart");}); // Called when a resource starts loading.
+loader.onError.add(() => {console.log("onError");}); // Called when a resource fails to load.
+loader.onLoad.add(() => {console.log("onLoad");}); // Called when a resource successfully loads.
+loader.onProgress.add(() => {console.log("onProgress");}); // Called when a resource finishes loading (success or fail).
+loader.onComplete.add(() => {console.log("onComplete");}); // Called when all resources have finished loading.
